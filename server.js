14d25b3535aaaf3f4ad888ce9ce331731e92b779
Jun Qin <jun.qin@outlook.com>
2015-08-24 11:45:53 +0800

    format server.js

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function(req, res) {
+app.get('/api/posts', function (req, res) {
     res.json([
         {
-            username : 'dickeyxxx',
-            body : 'node rocks!'
+            username: 'dickeyxxx',
+            body: 'node rocks!'
         }
     ]);
 });
 
-app.post('/api/posts', function(req, res){
+app.post('/api/posts', function (req, res) {
     console.log('post received!');
     console.log(req.body.username);
     console.log(req.body.body);
     res.send(201);
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Server listening on ', 3000);
 });
