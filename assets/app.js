f1f03e44cf3e8c86cb8e5e1da10ee6097d0590e7
Jun Qin <jun.qin@outlook.com>
2015-08-25 20:49:31 +0800

    re-org

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+var app = angular.module("app", []);
+app.controller("PostsCtrl", function ($scope, PostsSvc) {
+    $scope.addPost = function () {
+        if ($scope.postBody) {
+            PostsSvc.create({ username: 'jun.qin', body: $scope.postBody }).success(function (post) {
+                $scope.posts.unshift(post);
+                $scope.postBody = null;
+            });
+        }
+    };
+    $scope.posts = PostsSvc.fetch().success(function (posts) {
+        $scope.posts = posts;
+    });
+})
+
+app.service('PostsSvc', function ($http) {
+    this.fetch = function () {
+        return $http.get('/api/posts');
+    };
+    this.create = function (post) {
+        return $http.post('/api/posts', post);
+    }
+});
--- server.js
+++ server.js
@@ -1,33 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function (req, res) {
-    Post.find(function (err, posts) {
-        if (err) { return next(err); }
-        res.json(posts);
-    })
-});
-
-app.post('/api/posts', function (req, res) {
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    });
-    post.save(function (err, post) {
-        if (err) { return next(err) }
-        res.json(201, post)
-    });
-});
-
-app.get('/', function (req, res) {
-  res.sendfile('layouts/posts.html')
-});
-
 app.use("/node_modules", express.static('node_modules'));
+app.use('/api/posts', require('./controllers/api/posts'));
+app.use( require('./controllers/static'));
 
 app.listen(3000, function () {
     console.log('Server listening on ', 3000);
