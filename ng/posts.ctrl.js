01887cb7c2f45aac057d6812fa07dd8f8d5f9e59
Jun Qin <jun.qin@outlook.com>
2015-08-27 17:15:54 +0800

    added Helion files

--- ng/posts.ctrl.js
+++ ng/posts.ctrl.js
@@ -1,4 +1,3 @@
-var app = angular.module("app", []);
 angular.module("app").controller("PostsCtrl", function ($scope, PostsSvc) {
     $scope.addPost = function () {
         if ($scope.postBody) {
@@ -11,12 +10,4 @@ angular.module("app").controller("PostsCtrl", function ($scope, PostsSvc) {
     $scope.posts = PostsSvc.fetch().success(function (posts) {
         $scope.posts = posts;
     });
-});
-angular.module("app").service('PostsSvc', function ($http) {
-    this.fetch = function () {
-        return $http.get('/api/posts');
-    };
-    this.create = function (post) {
-        return $http.post('/api/posts', post);
-    }
 });
