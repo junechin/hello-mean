96d194ca0aadcba5b3b8b6f2dcedd7653a1ad40e
Jun Qin <jun.qin@outlook.com>
2015-09-01 10:59:38 +0800

    removed usage of deprecated methods

--- controllers/static.js
+++ controllers/static.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-      res.sendfile('layouts/posts.html')
+    res.sendFile(path.join(__dirname, '../layouts/posts.html'));
 });
 
 router.use(express.static(__dirname + '/../assets'));
